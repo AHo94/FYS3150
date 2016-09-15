31f1fe89638ecfc65d8ccf0ec3e7afbd0573fa09
AHo94 <hoalex94@gmail.com>
2016-09-15 12:52:59 +0200

    More compact method to define filenames

--- Project1/Project1_python/project_1_plotting_data.py
+++ Project1/Project1_python/project_1_plotting_data.py
@@ -3,36 +3,41 @@ import numpy as np
 import os
 
 file_directory = 'C:/Users/Alex/Documents/FYS3150/FYS3150_projects/Project1/build-Project1_cpp-Desktop_Qt_5_7_0_MinGW_32bit-Debug'
-filename_n10 = 'General_data_n10.txt'
-filename_n100 = 'General_data_n100.txt'
-filename_n1000 = 'General_data_n1000.txt'
-filename_simplified = 'Project1c_data_simplified.txt'
-filename_relative_error = 'Project1d_relative_error.txt'
-fn10 = open(os.path.join(file_directory, filename_n10), 'r')
-fn100 = open(os.path.join(file_directory, filename_n100), 'r')
-fn1000 = open(os.path.join(file_directory, filename_n1000), 'r')
-f_simplified = open(os.path.join(file_directory, filename_simplified), 'r')
-f_rel_error = open(os.path.join(file_directory, filename_relative_error), 'r')
 
-def save_and_plot(filename):
+general_points = np.array([10, 100, 1000])
+simplified_points = np.array([10, 100, 1000, 10000, 100000, 1000000])
+filename_general = []
+filename_simplified = []
+for general_n_pts in general_points:
+	filename_general.append('General_data_n'+str(general_n_pts)+'.txt')
+
+for simplified_n_pts in simplified_points:
+	filename_simplified.append('Simplified_data_n'+str(simplified_n_pts)+'.txt')
+
+def save_and_plot(filename_open, simplified_plot):
 	""" 
 	Function that reads the datafile, saves the values of n, x and v
 	Also solves the exact solution u(x) for the given values of x
 	Plots both v and u as a function of x
 	"""
+	filename = open(os.path.join(file_directory, filename_open), 'r')
 	i = 0
 	data = []
 	for line in filename:
 		n_value = line.split()
-		if i != 1:
+		if i != 0:
 			data.append(n_value)
 		i += 1
 	filename.close()
-	n = int(data[0][-1])
+	n = int(data[0][0])
+	step = int(data[0][1])
 	x = np.zeros(n)
 	v = np.zeros(n)
-
-	for j in range(0,n,1):
+	if step <= 1:
+		maxpoints = n
+	elif step > 1:
+		maxpoints = 1000
+	for j in range(0,maxpoints,1):
 		x[j] = float(data[j+1][0])
 		v[j] = float(data[j+1][1])
 
@@ -44,7 +49,7 @@ def save_and_plot(filename):
 	plt.xlabel('x')
 	plt.ylabel('u')
 	plt.legend(['Numerical solution', 'Exact solution'])
-	if filename == f_simplified:
+	if simplified_plot == True:
 		plt.title('Plot of $v$ and $u$, special algorithm, , n = %g' %(n))
 	else:
 		plt.title('Plot of $v$ and $u$, general algorithm, , n = %g' %(n))
@@ -69,6 +74,16 @@ def relative_error(filename):
 
 	u_exact = 1-(1-np.exp(-10))*x - np.exp(-10*x)
 
+for file_gen in filename_general:
+	plt.figure()
+	save_and_plot(file_gen, False)
+
+for file_simpl in filename_simplified:
+	plt.figure()
+	save_and_plot(file_simpl, True)
+
+plt.show()
+"""
 save_and_plot(fn10)
 plt.figure()
 save_and_plot(fn100)
@@ -77,5 +92,6 @@ save_and_plot(fn1000)
 plt.figure()
 save_and_plot(f_simplified)
 plt.figure()
-relative_error(f_rel_error)
+#relative_error(f_rel_error)
 plt.show()
+"""
