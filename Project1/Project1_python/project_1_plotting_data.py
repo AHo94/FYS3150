3715a2d1786991b89e1b32b351eaae42357b3fe0
AHo94 <hoalex94@gmail.com>
2016-09-15 13:29:03 +0200

    Added relative error plotting

--- Project1/Project1_python/project_1_plotting_data.py
+++ Project1/Project1_python/project_1_plotting_data.py
@@ -4,15 +4,24 @@ import os
 
 file_directory = 'C:/Users/Alex/Documents/FYS3150/FYS3150_projects/Project1/build-Project1_cpp-Desktop_Qt_5_7_0_MinGW_32bit-Debug'
 
+# Defines lists that contains the number of points used
+# Used for filename convention
 general_points = np.array([10, 100, 1000])
 simplified_points = np.array([10, 100, 1000, 10000, 100000, 1000000])
+error_points = np.array([10, 100, 1000, 10000, 100000, 1000000, 10000000])
 filename_general = []
 filename_simplified = []
+filename_error = []
+
+# For loops that creates filenames that is used for datafiles
 for general_n_pts in general_points:
 	filename_general.append('General_data_n'+str(general_n_pts)+'.txt')
 
 for simplified_n_pts in simplified_points:
 	filename_simplified.append('Simplified_data_n'+str(simplified_n_pts)+'.txt')
+
+for error_n_pts in error_points:
+	filename_error.append('Error_data_n'+str(error_n_pts)+'.txt')
 
 def save_and_plot(filename_open, simplified_plot):
 	""" 
@@ -54,26 +63,51 @@ def save_and_plot(filename_open, simplified_plot):
 	else:
 		plt.title('Plot of $v$ and $u$, general algorithm, , n = %g' %(n))
 
-def relative_error(filename):
+def relative_error(error_list):
 	""" Function that solves and plots the relative error """
-	i = 0
-	data = []
-	for line in filename:
-		n_value = line.split()
-		if i != 1:
-			data.append(n_value)
-		i += 1
-	filename.close()
-	n = int(data[0][-1])
-	x = np.zeros(n)
-	v = np.zeros(n)
+	#n_values = zeros(len(error_list))
+	h_values = np.zeros(len(error_list))
+	rel_error = np.zeros(len(error_list))
+	h_count = 0
+	for file_error in error_list:
+		filename = open(os.path.join(file_directory, file_error), 'r')
+		i = 0
+		data = []
+		for line in filename:
+			n_value = line.split()
+			if i != 0:
+				data.append(n_value)
+			i += 1
+		filename.close()
+		n = int(data[0][0])
+		step = int(data[0][1])
+		v = np.zeros(n)
+		x = np.zeros(n)
+		h = 1.0/(n-1)
 
-	for j in range(0,n,1):
-		x[j] = float(data[j+1][0])
-		v[j] = float(data[j+1][1])
+		if step <= 1:
+			maxpoints = n
+		elif step > 1:
+			maxpoints = 1000
 
-	u_exact = 1-(1-np.exp(-10))*x - np.exp(-10*x)
+		for j in range(0,maxpoints,1):
+			x[j] = float(data[j+1][0])
+			v[j] = float(data[j+1][1])
 
+		u_exact = 1-(1-np.exp(-10))*x - np.exp(-10*x)
+
+		h_values[h_count] = np.log(h)/np.log(10)
+		rel_error[h_count] = np.max(np.log(np.abs((v[1:999]-u_exact[1:999])/u_exact[1:999]))/np.log(10))
+		h_count += 1
+
+	
+	plt.figure()
+	plt.plot(h_values, rel_error, 'g-o')
+	plt.xlabel('$log_{10}(h)$')
+	plt.ylabel('Relative error, in $log_{10}$ scale')
+	plt.title('Relative error')
+# Plots the figures
+"""
 for file_gen in filename_general:
 	plt.figure()
 	save_and_plot(file_gen, False)
@@ -81,17 +115,6 @@ for file_gen in filename_general:
 for file_simpl in filename_simplified:
 	plt.figure()
 	save_and_plot(file_simpl, True)
-
-plt.show()
 """
-save_and_plot(fn10)
-plt.figure()
-save_and_plot(fn100)
-plt.figure()
-save_and_plot(fn1000)	
-plt.figure()
-save_and_plot(f_simplified)
-plt.figure()
-#relative_error(f_rel_error)
+relative_error(filename_error)	
 plt.show()
-"""
