0fde17b9b8382328085afb0106d0888713a94ecc
AHo94 <hoalex94@gmail.com>
2016-09-23 14:45:06 +0200

    Python program to plot the data

---
+++ Project2/Project2_python_program/Project_2_plotting.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+file_directory = 'C:/Users/Alex/Documents/FYS3150/FYS3150_projects/Project2/build-Project2_cpp_program-Desktop_Qt_5_7_0_MinGW_32bit-Debug'
+
+# Defines lists that contains the number of points used
+# Used for filename convention
+omega_value = np.array([0.01, 0.50, 1.00, 5.00])
+omega_filenames = []
+# For loops that creates filenames that is used for datafiles
+for omega_points in omega_value:
+	omega_filenames.append('Eigenvector_data_omega_'+str(omega_points)+'.txt')
+
+
+
+def save_and_plot(filename_open):
+	filename = open(os.path.join(file_directory, filename_open), 'r')
+	i = 0
+	data = []
+	for line in filename:
+		n_value = line.split()
+		if i >= 2:
+			data.append(n_value)
+		i += 1
+	filename.close()
+	n = int(data[0][0])
+	rho_max = float(data[0][1])
+	rho = np.zeros(n+2)
+	rho[-1] = rho_max
+	eig1 = np.zeros(n+2)
+	eig2 = np.zeros(n+2)
+	eig3 = np.zeros(n+2)
+	for j in range(0, n, 1):
+		rho[j+1] = float(data[j+1][0])
+		eig1[j+1] = float(data[j+1][1])
+		eig2[j+1] = float(data[j+1][2])
+		eig3[j+1] = float(data[j+1][3])
+
+	plt.plot(rho, np.abs(eig1)**2, 'b-')
+	plt.xlabel(r'$\rho$')
+	plt.ylabel('$|\psi|^2$')
+
+save_and_plot(omega_filenames[0])
+"""
+fig_err = plt.figure()
+relative_error(filename_error)
+fig_err.savefig('../Data_plots/Relative_error.png')
+"""
+plt.show()
