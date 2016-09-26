a03598da8584419708653e471128b01011c15a6f
AHo94 <hoalex94@gmail.com>
2016-09-26 17:50:39 +0200

    Saving only the ground state eigenvector

--- Project2/Project2_cpp_program/main.cpp
+++ Project2/Project2_cpp_program/main.cpp
@@ -167,13 +167,13 @@ void write_file(double **R, double *rho, double rho_max, double omega, int n, in
     int min3 = vector_index[2];
     datafile << "# First row contains the n, rho_max and omega values respectively.";
     datafile << " The other rows contains the data to be plotted \n";
-    datafile << "# First column contains rho values. The other values contains smallest 3 eigenvectors \n";
+    datafile << "# First column contains rho values. Second column contains the eigenvector for the ground state";
     datafile << n << setw(15) << rho_max << setw(15) << omega << "\n";
     for (int i=0; i<n; i++){
         datafile << rho[i] << setw(15)
-                 << pow(R[i][min1], 2) << setw(15)
-                 << pow(R[i][min2] ,2) << setw(15)
-                 << pow(R[i][min3], 2) << "\n";
+                 << pow(R[i][min1], 2) << "\n"; // setw(15)
+                 //<< pow(R[i][min2] ,2) << setw(15)
+                 //<< pow(R[i][min3], 2) << "\n";
     }
     datafile.close();
 }
