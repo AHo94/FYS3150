d25539acff8e3b40393b10ba46eb5db1eea50318
AHo94 <hoalex94@gmail.com>
2016-10-06 16:07:10 +0200

    Major changes to the main program

--- Project3/Project3_cpp_program/main.cpp
+++ Project3/Project3_cpp_program/main.cpp
@@ -1,38 +1,58 @@
 #include <iostream>
 #include <cmath>    // Math library
-#include <fstream>  // Used to read files
-#include <iomanip>  // setw identitation for output file
-#include <string>
-#include "eulermethod.h"
 #include "vec3.h"
 #include "celestials.h"
-
+#include "solarsystem.h"
+#include "odesolvers.h"
 using namespace std;
 
 int main()
 {
     // Masses of the celestial bodies in the solar system
-    eulermethod Euler_solver;
-    double M_sun, M_earth, M_jupiter, M_mars;
-    M_sun = 2*pow(10,30);
-    M_earth = 6*pow(10,24);
-    M_jupiter = 1.9*pow(10,27);
-    M_mars = 6.6*pow(10,23);
 
-    double AU = 1.5*pow(10,11); // Defining one astronomical unit in meters
-    double R_earth = 1*AU;
+    SolarSystem System;
+    double M_sun, M_earth, M_jupiter, YrstoDays;
+    YrstoDays = 1.0/365;  // Converts one year to 365 days
+    M_sun = 1.0;
+    M_earth = 6*pow(10,24)/(2*pow(10,30));
+    M_jupiter = 1.9*pow(10,27)/(2*pow(10,30));
 
-    Celestials Earth;
-    Earth.set_properties(M_earth, R_earth);
-    cout << "Mass of Earth = " << Earth.get_mass() << endl;;
+    System.createCelestialBody(vec3(0,0,0), vec3(0,0,0), M_sun);
+    vec3 Earthpos (9.890331046925951E-01, 1.768079890757788E-01, -1.738715302893284E-04);
+    vec3 Earthvel (-3.268395786841218E-03, 1.689265025904021E-02, -9.889230545039174E-07);
+    Earthvel /= YrstoDays;
+    System.createCelestialBody(Earthpos, Earthvel, M_earth);
 
-    vec3 Earth_pos;
+    double dt = 0.01;
+    int NumTimesteps = 1000;
+    ODEsolvers solver(dt);
+    for (int step=0; step<NumTimesteps; step++){
+        System.write_file("TESTINGFILE.txt");
+        //solver.Euler_step(System);
+        //solver.EulerCromer(System);
+        solver.Verlet(System);
+    }
+
+
+    //Celestials Earth(1,0,0,0,0,1);
+    /*
     string earth_file = "../NASA_data/horizons_results.txt";
+
     std::ifstream infile(earth_file);
     std::string line;
+    std::string delimeter = "  ";
     char c;
     int counter = 0;
     while (std::getline(infile, line)){
+        std::stringstream ss(line);
+
+        int n;
+        std::vector<int> v;
+        while (iss > n);
+        {
+            v.push_back(n);
+        }
+
         if (line == "$$SOE"){
             counter += 1;
         }
@@ -40,31 +60,11 @@ int main()
             counter -= 1;
         }
         if (counter >= 1 && line != "$$SOE"){
-            cout << line << endl;
+            cout << std:: line << endl;
         }
+
     }
-    double **Epos, **Evel;
-    int n = 1000;
-    Epos = new double*[2];
-    Evel = new double*[2];
-    for (int i=0; i<2; i++){
-        Epos[i] = new double[n];
-        Evel[i] = new double[n];
-    }
-    Epos[0][0] = 1.479572465138224E+08;
-    Epos[1][0] = 2.645009868848537E+07;
-    Evel[0][0] = -5.659086230512699E+00;
-    Evel[1][0] = 2.924889478278030E+01;
-    Euler_solver.solve_euler_2D(Epos, Evel, n);
-    ofstream datafile;
-    datafile.open("TESTFILE.txt");
-    for (int i=0; i<n; i++){
-        datafile << Epos[0][i] << setw(15)
-                 << Epos[1][i] << setw(15)
-                 << Evel[0][i] << setw(15)
-                 << Evel[1][i] << "\n";
-    }
-    datafile.close();
+    */
 
     return 0;
 }
