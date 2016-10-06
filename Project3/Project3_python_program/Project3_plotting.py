a12ba3dd1a08be9a90f826a63746313cbe69621a
AHo94 <hoalex94@gmail.com>
2016-10-06 21:24:51 +0200

    Added method to save as a figure if needed. Also added titles

--- Project3/Project3_python_program/Project3_plotting.py
+++ Project3/Project3_python_program/Project3_plotting.py
@@ -4,9 +4,10 @@ import os
 file_directory = 'C:/Users/Alex/Documents/FYS3150/FYS3150_projects/Project3/build-Project3_cpp_program-Desktop_Qt_5_7_0_MinGW_32bit-Debug'
 
 class Plotter():
-	def __init__(self):
+	def __init__(self, savefile):
 		self.filename = "Celestial_positions.txt"
 		self.read_data(self.filename)
+		self.savefile = savefile
 
 	def read_data(self, filename_open):
 		filename = open(os.path.join(file_directory, filename_open), 'r')
@@ -21,7 +22,6 @@ class Plotter():
 		NumberofObjects = (len(data[0])/3.0)
 		self.Sun_pos = np.zeros((3,self.N))
 		self.Earth_pos = np.zeros((3,self.N))
-		#if NumberofObjects <= 3:
 		self.Jupiter_pos = np.zeros((3, self.N))
 		self.Mercury_pos = np.zeros((3, self.N))
 		self.Mars_pos = np.zeros((3, self.N))
@@ -40,34 +40,44 @@ class Plotter():
 				self.Uranus_pos[i][j] = data[j][i+18]
 				self.Neptune_pos[i][j] = data[j][i+21]
 
-	def Earth_Jupiter_test(self):			
-		plt.plot(self.Earth_pos[0][:], self.Earth_pos[1][:])
-		plt.hold("on")
-		plt.plot(self.Sun_pos[0][:], self.Sun_pos[1][:])
-		plt.plot(self.Jupiter_pos[0][:], self.Jupiter_pos[1][:])
-		plt.xlabel('x - [AU]')
-		plt.ylabel('y - [AU]')
-		plt.legend(['Earth', 'Sun', 'Jupiter'])
-		plt.show()
+	def Earth_Jupiter_test(self):	
+		fig = figure()
+		if savefile:
+			fig.savefig('../Plots/'+'Earth_Jupiter_test.pdf')
+		else:		
+			plt.plot(self.Earth_pos[0][:], self.Earth_pos[1][:])
+			plt.hold("on")
+			plt.plot(self.Sun_pos[0][:], self.Sun_pos[1][:])
+			plt.plot(self.Jupiter_pos[0][:], self.Jupiter_pos[1][:])
+			plt.xlabel('x - [AU]')
+			plt.ylabel('y - [AU]')
+			plt.legend(['Earth', 'Sun', 'Jupiter'])
+			plt.title('Orbits for a system with the Sun, Earth and Jupiter.')
+			plt.show()
 
 	def plotting_2D(self):
-		plt.plot(self.Earth_pos[0][:], self.Earth_pos[1][:])
-		plt.hold("on")
-		plt.plot(self.Sun_pos[0][:], self.Sun_pos[1][:])
-		plt.plot(self.Jupiter_pos[0][:], self.Jupiter_pos[1][:])
-		plt.plot(self.Mercury_pos[0][:], self.Mercury_pos[1][:])
-		plt.plot(self.Mars_pos[0][:], self.Mars_pos[1][:])
-		plt.plot(self.Saturn_pos[0][:], self.Saturn_pos[1][:])
-		plt.plot(self.Uranus_pos[0][:], self.Uranus_pos[1][:])
-		plt.plot(self.Neptune_pos[0][:], self.Neptune_pos[1][:])
-		plt.xlabel('x - [AU]')
-		plt.ylabel('y - [AU]')
-		plt.legend(['Earth', 'Sun', 'Jupiter', 'Mercury', 'Mars', 'Saturn', 'Uranus', 'Neptune'])
-		plt.show()
+		fig = figure()
+		if savefile:
+			fig.savefig('../Plots/'+'All_planets_plot.pdf')
+		else:
+			plt.plot(self.Earth_pos[0][:], self.Earth_pos[1][:])
+			plt.hold("on")
+			plt.plot(self.Sun_pos[0][:], self.Sun_pos[1][:])
+			plt.plot(self.Jupiter_pos[0][:], self.Jupiter_pos[1][:])
+			plt.plot(self.Mercury_pos[0][:], self.Mercury_pos[1][:])
+			plt.plot(self.Mars_pos[0][:], self.Mars_pos[1][:])
+			plt.plot(self.Saturn_pos[0][:], self.Saturn_pos[1][:])
+			plt.plot(self.Uranus_pos[0][:], self.Uranus_pos[1][:])
+			plt.plot(self.Neptune_pos[0][:], self.Neptune_pos[1][:])
+			plt.xlabel('x - [AU]')
+			plt.ylabel('y - [AU]')
+			plt.legend(['Earth', 'Sun', 'Jupiter', 'Mercury', 'Mars', 'Saturn', 'Uranus', 'Neptune'])
+			plt.title('Orbits of the planets. Timescale in years.')
+			plt.show()
 
 	def animation(self):
 		# Animates 
 		a = 3
 
 solve = Plotter()
-solve.plotting_2D()
+solve.plotting_2D(False)
