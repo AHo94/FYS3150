cae9529891050fdc6cf364a12aeeedc20cee868e
AHo94 <hoalex94@gmail.com>
2016-10-06 16:43:54 +0200

    Adding Jupiter to simulation

--- Project3/Project3_python_program/Project3_plotting.py
+++ Project3/Project3_python_program/Project3_plotting.py
@@ -12,32 +12,35 @@ class Plotter():
 	def read_data(self, filename_open):
 		filename = open(os.path.join(file_directory, filename_open), 'r')
 		i = 0
-		self.data = []
+		data = []
 		for line in filename:
 			data_set = line.split()
 			data.append(data_set)
 		filename.close()
 
-		self.N = len(self.data)
-		NumberofObjects = (len(self.data[0])/3.0)
+		self.N = len(data)
+		NumberofObjects = (len(data[0])/3.0)
 		self.Sun_pos = np.zeros((3,self.N))
 		self.Earth_pos = np.zeros((3,self.N))
-		if NumberofObjects <= 3:
-			self.Jupiter = np.zeros((3, self.N))
+		#if NumberofObjects <= 3:
+		self.Jupiter_pos = np.zeros((3, self.N))
 
 		for i in range(0,3):
 			for j in range(0,self.N):
-				self.Earth_pos[i][j] = data[j][i+3]
 				self.Sun_pos[i][j] = data[j][i]
+				self.Earth_pos[i][j] = data[j][i+3]
+				self.Jupiter_pos[i][j] = data[j][i+6]
 
 
 
 	def plotting(self):
-		plt.plot(self.Earth_pos[0][:], self.Earth_pos[1][:], 'b-')
+		plt.plot(self.Earth_pos[0][:], self.Earth_pos[1][:])
 		plt.hold("on")
-		plt.plot(self.Sun_pos[0][:], self.Sun_pos[1][:], 'r-')
+		plt.plot(self.Sun_pos[0][:], self.Sun_pos[1][:])
+		plt.plot(self.Jupiter_pos[0][:], self.Jupiter_pos[1][:])
 		plt.xlabel('x - [AU]')
 		plt.ylabel('y - [AU]')
+		plt.legend(['Earth', 'Sun', 'Jupiter'])
 		plt.show()
 
 	def animation(self):
