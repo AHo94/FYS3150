f41f3d523c17e92ad7e84ff89270d496c92c4955
AHo94 <hoalex94@gmail.com>
2016-10-07 15:01:41 +0200

    Attempting to implement animation

--- Project3/Project3_python_program/Project3_plotting.py
+++ Project3/Project3_python_program/Project3_plotting.py
@@ -2,6 +2,7 @@ import matplotlib as mpl
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import animation
 import os
 file_directory = 'C:/Users/Alex/Documents/FYS3150/FYS3150_projects/Project3/build-Project3_cpp_program-Desktop_Qt_5_7_0_MinGW_32bit-Debug'
 
@@ -10,6 +11,7 @@ class Plotter():
 		self.filename = "Celestial_positions.txt"
 		self.read_data(self.filename)
 		self.savefile = savefile
+
 
 	def read_data(self, filename_open):
 		filename = open(os.path.join(file_directory, filename_open), 'r')
@@ -108,16 +110,51 @@ class Plotter():
 		ax.scatter(self.Saturn_pos[0][0], self.Saturn_pos[1][0], self.Saturn_pos[2][0], color='green', s=150)
 		ax.scatter(self.Neptune_pos[0][0], self.Neptune_pos[1][0], self.Neptune_pos[2][0], color='green', s=90)
 		ax.scatter(self.Uranus_pos[0][0], self.Uranus_pos[1][0], self.Uranus_pos[2][0], color='green', s=100)
-		ax.scatter(self.Pluto_pos[0][0], self.Pluto_pos[1][0], self.Pluto_pos[2][0], color='green', s=20)
+		ax.scatter(self.Pluto_pos[0][0], self.Pluto_pos[1][0], self.Pluto_pos[2][0], color='green', s=5)
 		#ax.add_artist(circle_Jupiter)
 		if self.savefile:
 			fig.savefig('../Plots/'+'All_planets_3D_plot.pdf')
 		else:
 			plt.show()
 
-	def animation(self):
+	def animate(self):
 		# Animates 
-		a = 1
+		fig = plt.figure()
+		#ax = fig.gca(projection='3d')
+		ax = fig.add_subplot(111, projection='3d')
+		ax.set_xlabel('X - [AU]')
+		ax.set_ylabel('Y - [AU]')
+		ax.set_zlabel('Z - [AU]')
 
+		theta, phi = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+		Jx = 0.1*np.cos(theta)*np.sin(phi)+self.Jupiter_pos[0][0]
+		Jy = 0.1*np.sin(theta)*np.sin(phi)+self.Jupiter_pos[1][0]
+		Jz = 0.1*np.cos(phi)+self.Jupiter_pos[2][0]
+		Jsphere =ax.plot_wireframe(Jx,Jy,Jz)
+		Sx = 0.6*np.cos(theta)*np.sin(phi)+self.Sun_pos[0][0]
+		Sy = 0.6*np.sin(theta)*np.sin(phi)+self.Sun_pos[1][0]
+		Sz = 0.6*np.cos(phi)+self.Sun_pos[2][0]
+		#Ssphere = ax.plot_wireframe(Sx, Sy, Sz, color='r')
+		#plt.show()
+		#jupiter = ax.scatter(self.Jupiter_pos[0][0], self.Jupiter_pos[1][0], self.Jupiter_pos[2][0], s=100, animated=True)
+		
+		def init():
+			return
+
+		def update(i):
+			#jupiter = ax.scatter(self.Jupiter_pos[0][i], self.Jupiter_pos[1][i], self.Jupiter_pos[2][i], s=100, animated=True)
+			#jupiter._offsets3D(self.Jupiter_pos[0][i], self.Jupiter_pos[1][i], self.Jupiter_pos[2][i])
+			#jupiter.set_data(self.Jupiter_pos[0][i], self.Jupiter_pos[1][i], self.Jupiter_pos[2][i])
+			Jx = 0.1*np.cos(theta)*np.sin(phi)+self.Jupiter_pos[0][i]
+			Jy = 0.1*np.sin(theta)*np.sin(phi)+self.Jupiter_pos[1][i]
+			Jz = 0.1*np.cos(phi)+self.Jupiter_pos[2][i]
+			Jsphere = ax.plot_wireframe(Jx, Jy, Jz, animated=True)
+			return Jsphere,
+
+
+		anim = animation.FuncAnimation(fig, update, frames=300, interval=20, blit=True)
+		
+		plt.show()
 solve = Plotter(False)
-solve.plotting_3D()
+#solve.plotting_3D()
+solve.animate()
