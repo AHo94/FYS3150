9c5e98763cb48c35317582574a9cebd54ab94d9a
AHo94 <hoalex94@gmail.com>
2016-11-08 19:11:36 +0100

    Now plots E and M for both temperatures and inlcudes histogram plot for 4d

--- Project4/Project4_python_program/Project4_plotting.py
+++ Project4/Project4_python_program/Project4_plotting.py
@@ -34,15 +34,54 @@ class Plotter():
 			self.E_mean_T24[i] = float(data[i+N_half][4])
 			self.M_mean_T1[i] = float(data[i][6])
 			self.M_mean_T24[i] = float(data[i+N_half][6])
-			
+
+	def read_data_4c(self, filename_E, filename_M):
+		filename = open(os.path.join(file_directory, filename_E), 'r')
+		i = 0
+		data_E = []
+		for line in filename:
+			if i != 0:
+				data_set = line.split()
+				data_E.append(data_set)
+			i += 1
+		filename.close()
+
+		filename = open(os.path.join(file_directory, filename_M), 'r')
+		i = 0
+		data_M = []
+		for line in filename:
+			if i != 0:
+				data_set = line.split()
+				data_M.append(data_set)
+			i += 1
+		filename.close()
+		N = len(data_E)-1
+		MC_max = int(float(data_E[0][0]))
+		self.L = float(data_E[0][1])
+		self.T = float(data_E[0][2])
+		self.MC_cycles = np.linspace(1, MC_max, N)
+		self.E_mean = np.zeros(N)
+		self.M_mean = np.zeros(N)
+		for i in range(0, N):
+			self.E_mean[i] = float(data_E[i+1][0])
+			self.M_mean[i] = float(data_M[i+1][0])
+
 	def plot_state(self):
-		self.read_data("4c.txt")
-		plt.plot(np.log(self.MC_cycles), self.E_mean_T1, 'b-')
-		plt.hold("on")
-		plt.plot(np.log(self.MC_cycles), self.M_mean_T1, 'r-')
+		self.read_data_4c("Mean_E_T1.00.txt", "Mean_M_T1.00.txt")
+		plt.plot(np.log(self.MC_cycles), self.E_mean, 'b-')
 		plt.xlabel('$log(N_{MC})$')
-		plt.ylabel(r'$\langle  E\rangle$')
+		plt.ylabel(r'$\langle  E \rangle$')
+		plt.figure()
+		plt.plot(np.log(self.MC_cycles), self.M_mean, 'r-')
+		plt.xlabel('$log(N_{MC})$')
+		plt.ylabel(r'$\langle  |M| \rangle$')
+		plt.show()
+		
+	def plot_probability(self):
+		self.read_data_4c("Mean_E_T1.00.txt", "Mean_M_T1.00.txt")
+		plt.hist(self.E_mean, bins=30)
 		plt.show()
 
 solver = Plotter(False)
-solver.plot_state()
+#solver.plot_state()
+solver.plot_probability()
