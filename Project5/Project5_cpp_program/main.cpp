3fd88578b2b1d1d1e3d99a8f4bea5f18df65f62f
AHo94 <hoalex94@gmail.com>
2016-11-30 11:50:12 +0100

    Now calculates mean distance

--- Project5/Project5_cpp_program/main.cpp
+++ Project5/Project5_cpp_program/main.cpp
@@ -22,6 +22,7 @@ double StepLength(vec3 r1, vec3 r2, double alpha, double omega, double r){
 }
 
 double LaplaceOperator(vec3 r1, vec3 r2, double alpha, double omega){
+    // Function that calculates the Laplace operator numerically
     double SecondDerivative = 0;
     double dr = 1e-5;
     double wavefunc =  TrialWavefunc_T1(r1, r2, alpha, omega);
@@ -37,13 +38,10 @@ double LaplaceOperator(vec3 r1, vec3 r2, double alpha, double omega){
 }
 
 double LaplaceAnalytic(vec3 r1, vec3 r2, double alpha, double omega){
+    // Function that calculates the analytical expression of the Laplace operator
     double AlphaOmega = alpha*omega;
     return  -0.5*(pow(AlphaOmega,2)*r1.lengthSquared() - 3*AlphaOmega) \
             - 0.5*(pow(AlphaOmega,2)*r2.lengthSquared() - 3*AlphaOmega);
-}
-
-double LocalEnergy(vec3 r1, vec3 r2, double alpha, double omega){
-
 }
 
 void Metropolis_method(int MC_cycles, double omega, double alpha, double step_length, double *H_expect){
@@ -58,15 +56,17 @@ void Metropolis_method(int MC_cycles, double omega, double alpha, double step_le
     double E_local = 0;
     double EnergySum = 0;
     double EnergySquaredSum = 0;
+    double MeanDistance = 0;
     double NewWavefuncSquared = 0;
     double omega2 = omega*omega;
     double OldWavefuncSquared = pow(TrialWavefunc_T1(r1, r2, alpha, omega), 2);
 
     //double step_length = StepLength(r1, r2, alpha, omega, distr(generator));
-
+    // Analytic expression as a comparison
     double E_an = 0.5*omega2*(r1.lengthSquared() + r2.lengthSquared())*(1-alpha*alpha) + 3*alpha*omega;
     int counter = 0;
     for (int cycle=0; cycle<MC_cycles; cycle++){
+        // Running Monte Carlo cycles
         vec3 r1_new(0,0,0);
         vec3 r2_new(0,0,0);
         for (int j=0; j<3; j++){
@@ -85,9 +85,11 @@ void Metropolis_method(int MC_cycles, double omega, double alpha, double step_le
         E_local = LaplaceOperator(r1, r2, alpha, omega) + 0.5*omega2*(r1.lengthSquared() + r2.lengthSquared());
         EnergySum += E_local;
         EnergySquaredSum += E_local*E_local;
+        MeanDistance += sqrt(r1.lengthSquared() + r2.lengthSquared());
     }
     H_expect[0] = EnergySum;
     H_expect[1] = EnergySquaredSum;
+    H_expect[2] = MeanDistance;
     cout << "E = "<< H_expect[0]/(MC_cycles) << endl;
     cout << "E analytic = " << E_an << endl;
     cout << "Variance = " << EnergySquaredSum/MC_cycles - EnergySum*EnergySum/MC_cycles/MC_cycles << endl;;
@@ -97,30 +99,36 @@ void Metropolis_method(int MC_cycles, double omega, double alpha, double step_le
 void write_file(int MC_cycles, double *H_expectation, double alpha){
     // Writes out data to the output file. Function made specific for the parallellization part.
     double Energy = H_expectation[0]/MC_cycles;
-    ofile_global << setw(15) << alpha ;
-    ofile_global << setw(15) << Energy << endl;;
+    double MeanDistance = H_expectation[2]/MC_cycles;
+    ofile_global << setw(15) << alpha;
+    ofile_global << setw(15) << Energy;
+    ofile_global << setw(15) << MeanDistance << endl;
 }
 
 void initialize_outfile(){
     ofile_global << setw(15) << "alpha";
-    ofile_global << setw(15) << "Energy" << endl;
+    ofile_global << setw(15) << "Energy";
+    ofile_global << setw(15) << "Mean Distance" << endl;
 }
 
 int main(int argc, char *argv[])
 {
     double *H_expect;
-    H_expect = new double [2];
+    H_expect = new double [3];
     int MC_cycles = 100000;
     double omega, alpha, step_length;
-    omega = 1;
-    //alpha = 1;
-    step_length = 0.005;
+    omega = 0.01;
+    alpha = 1;
+    step_length = 0.001;
+    Metropolis_method(MC_cycles, omega, alpha, step_length, H_expect);
+    /*
     ofile_global.open("Energy_alpha.txt");
     initialize_outfile();
-    for (alpha = 0.2; alpha<=10; alpha +=0.1){
+    for (alpha = 0.7; alpha<=2; alpha +=0.1){
         Metropolis_method(MC_cycles, omega, alpha, step_length, H_expect);
         write_file(MC_cycles, H_expect, alpha);
     }
     ofile_global.close();
+    */
     return 0;
 }
