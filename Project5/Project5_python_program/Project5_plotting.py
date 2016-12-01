8d393db120a915e1967af7a5ae6da5f40dafbd7a
AHo94 <hoalex94@gmail.com>
2016-12-01 11:26:08 +0100

    Plotting now picks up omega

--- Project5/Project5_python_program/Project5_plotting.py
+++ Project5/Project5_python_program/Project5_plotting.py
@@ -15,21 +15,23 @@ class Plotter():
 		i = 0
 		data = []
 		for line in filename:
+			data_set = line.split()
 			if i != 0:
-				data_set = line.split()
 				data.append(data_set)
 			i += 1
 		filename.close()
 
 		N = len(data)
+		self.omega = float(data[0][-1])
 		self.Energy = np.zeros(N)
 		self.alpha = np.zeros(N)
-		self.Energy_Exact = np.ones(N)*3
+		self.Energy_Exact = 3*self.omega*np.ones(N)
 		self.MeanDistance = np.zeros(N)
 		for j in range(0, N):
 			self.alpha[j] = float(data[j][0])
 			self.Energy[j] = float(data[j][1])
 			self.MeanDistance[j] = float(data[j][2])
+
 
 	def plot_energy_alpha(self):
 		self.read_data("Energy_Alpha_Mdistance.txt")
@@ -39,14 +41,14 @@ class Plotter():
 		plt.plot(self.alpha, self.Energy_Exact)
 		plt.xlabel(r'$\alpha$')
 		plt.ylabel(r'$\langle H \rangle$')
-		plt.title(r'Plot of the $\langle H \rangle$ as a function of $\alpha$ with $\omega$ = 1. Using $\psi_{T_1}$')
+		plt.title(r'Plot of the $\langle H \rangle$ as a function of $\alpha$ with $\omega$ = %.g. Using $\psi_{T_1}$' %(self.omega))
 		plt.legend(['Numerical value','Exact'])
 
 		fig2 = plt.figure()
 		plt.plot(self.alpha, self.MeanDistance)
 		plt.xlabel(r'$\alpha$')
 		plt.ylabel(r'$r_{12}$')
-		plt.title(r'Plot of the mean distance $r_{12}$ as a function of $\alpha$ with $\omega$ = 1')
+		plt.title(r'Plot of the mean distance $r_{12}$ as a function of $\alpha$ with $\omega$ = %.g' %(self.omega))
 		if self.savefile == True:
 			fig1.savefig('../Plots/Energy_alpha_plot.pdf')
 			fig2.savefig('../Plots/MeanDistance_alpha_plot.pdf')
