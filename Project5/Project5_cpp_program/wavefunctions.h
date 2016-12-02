3a58209d2f97f806129eb211c74f2e469763a2fe
AHo94 <hoalex94@gmail.com>
2016-12-02 18:26:12 +0100

    Beta now as an optional paramete

--- Project5/Project5_cpp_program/wavefunctions.h
+++ Project5/Project5_cpp_program/wavefunctions.h
@@ -7,7 +7,7 @@ class Wavefunctions
 public:
     Wavefunctions(int WaveFuncChoice);
     int FuncFactor;
-    double operator()(vec3 r1, vec3 r2, double alpha, double beta, double omega)\
+    double operator()(vec3 r1, vec3 r2, double omega, double alpha, double beta=0)\
                 {return (FuncFactor*Wavefunction_T1(r1, r2, alpha, omega) \
                 + (1-FuncFactor)*Wavefunction_T2(r1, r2, alpha, beta, omega));}
     double Wavefunction_T1(vec3 r1, vec3 r2, double alpha, double omega);
